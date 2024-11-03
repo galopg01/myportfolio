0e4580d81bf9b4fcab27f269d215ac16f10171da
Galo <galopg2001@hotmail.com>
2024-11-03 03:03:41 +0100

    Home page design

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,461 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
+import "./globals.css";
+// @ts-ignore
+import { Link as ScrollLink } from "react-scroll";
+import Link from "next/link";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [activeSection, setActiveSection] = useState("about");
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const handleSetActiveSection = (section: string) => {
+    setActiveSection(section);
+  };
+
+  return (
+    <div className="bg-slate-900 leading-relaxed text-slate-400 selection:bg-teal-300 selection:text-teal-900">
+      <div
+        className="pointer-events-none fixed inset-0 z-30 transition duration-300 lg:absolute"
+        style={{
+          background:
+            "radial-gradient(600px circle at 0px 0px, rgba(29, 78, 216, 0.15), transparent 80%)",
+        }}
+      ></div>
+      <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 md:px-12 md:py-20 lg:px-24 lg:py-0">
+        <div className="lg:flex lg:justify-between lg:gap-4">
+          <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24">       
+            <div>
+              <h1 className="text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl">
+                <Link href="/">
+                Galo Pérez Gallego
+                </Link>
+              </h1>
+              <h2 className="mt-3 text-lg font-medium tracking-tight text-slate-200 sm:text-xl">
+                Software Engineer
+              </h2>
+              <p className="mt-4 max-w-xs leading-normal">
+                I build pixel-perfect, engaging, and accessible digital experiences.
+              </p>
+              <nav
+                className="nav hidden lg:block"
+                aria-label="In-page jump links"
+              >
+                <ul className="mt-16 w-max">
+                  <li>
+                    <ScrollLink
+                      className="group flex items-center py-3 cursor-pointer"
+                      to="about"
+                      smooth={true}
+                      offset={-100}
+                      duration={500}
+                      spy={true}
+                      onSetActive={() => handleSetActiveSection("about")}
+                    >
+                      <span className={`nav-indicator ${activeSection === "about" ? "w-16 bg-slate-200" : "w-8 bg-slate-600"} mr-4 h-px transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none`}>
+                      </span>
+                      <span className={`nav-text text-xs font-bold uppercase tracking-widest ${activeSection === "about" ? "text-slate-200" : "text-slate-500"} group-hover:text-slate-200 group-focus-visible:text-slate-200`}>
+                        About
+                      </span>
+                    </ScrollLink>
+                  </li>
+                  <li>
+                    <ScrollLink
+                      className="group flex items-center py-3 cursor-pointer"
+                      to="experience"
+                      smooth={true}
+                      duration={500}
+                      offset={-100}
+                      spy={true}
+                      onSetActive={() => handleSetActiveSection("experience")}
+                    >
+                      <span className={`nav-indicator ${activeSection === "experience" ? "w-16 bg-slate-200" : "w-8 bg-slate-600"} mr-4 h-px transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none`}>
+                      </span>
+                      <span className={`nav-text text-xs font-bold uppercase tracking-widest ${activeSection === "experience" ? "text-slate-200" : "text-slate-500"} group-hover:text-slate-200 group-focus-visible:text-slate-200`}>
+                        Experience
+                      </span>
+                    </ScrollLink>
+                  </li>
+                  <li>
+                    <ScrollLink
+                      className="group flex items-center py-3 cursor-pointer"
+                      to="projects"
+                      smooth={true}
+                      duration={500}
+                      offset={-100}
+                      spy={true}
+                      onSetActive={() => handleSetActiveSection("projects")}
+                    >
+                      <span className={`nav-indicator ${activeSection === "projects" ? "w-16 bg-slate-200" : "w-8 bg-slate-600"} mr-4 h-px transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none`}>
+                      </span>
+                      <span className={`nav-text text-xs font-bold uppercase tracking-widest ${activeSection === "projects" ? "text-slate-200" : "text-slate-500"} group-hover:text-slate-200 group-focus-visible:text-slate-200`}>
+                        Projects
+                      </span>
+                    </ScrollLink>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+            <ul className="ml-1 mt-8 flex items-center" aria-label="Social media">
+              <li className="mr-5 text-xs shrink-0">
+                <a
+                  href="https://twitter.com/galoperezgal"
+                  className="text-slate-200 hover:text-slate-100"
+                >
+                  X
+                </a>
+              </li>
+              <li className="mr-5 text-xs shrink-0">
+                <a
+                  href=""
+                  className="text-slate-200 hover:text-slate-100"
+                >
+                  GitHub
+                </a>
+              </li>
+              <li className="mr-5 text-xs shrink-0">
+                <a
+                  href=""
+                  className="text-slate-200 hover:text-slate-100"
+                >
+                  LinkedIn
+                </a>
+              </li>
+            </ul>
+          </header>
+          <main className="pt-24 lg:w-1/2 lg:py-24">
+            <section id="about" className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24">
+              <h2 className="text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only">About</h2>
+              <p className="mt-4 text-slate-400">Descripción sobre la carrera profesional y experiencia.</p>
+              <p>sdfdsssssssssssssssssssssssssssssssssssssssssssssss
+                asdfaf
+                asdfafasdf
+                ddafsdf
+                gfd
+                gsfdg
+                fsd
+                sd
+                sgf
+                ddafsdf
+                gfd
+                gsfdg
+                fsd
+                sd
+                sgf
+                dfg
+                dfg
+                dfg
+                fdgs
+                sdgf
+                sdagf
+                s
+                dsfdasg
+                dsfdasgasdgf
+                afdg
+                ag
+                sdgfadg
+                agrf
+                sdf
+                afdgadrgf
+                afdgadrgfadg
+                dsfdasgasdgf
+                sdgfadgfg
+                fg
+                dfg
+                sdgf
+                dfg
+                ddafsdf
+                gfd
+                gsfdg
+                fsd
+                sd
+                sgf
+                dfg
+                dfg
+                dfg
+                fdgs
+                sdgf
+                sdagf
+                s
+                dsfdasg
+                dsfdasgasdgf
+                afdg
+                ag
+                sdgfadg
+                agrf
+                sdf
+                afdgadrgf
+                afdgadrgfadg
+                dsfdasgasdgf
+                sdgfadgfg
+                fg
+                dfg
+                sdgf
+                dfg
+                dfg
+                dfg
+                dfg
+                fdgs
+                sdgf
+                sdagf
+                s
+              </p>
+            </section>
+            <section id="experience" className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24">
+              <h2 className="text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only">Experience</h2>
+              <p className="mt-4 text-slate-400">Descripción de la experiencia profesional.</p>
+              <p>sdfdssssssssssssssssssssssssssssssssssssssss
+                asdfaf
+                asdfafasdf
+                ddafsdf
+                gfd
+                gsfdg
+                fsd
+                sd
+                sgf
+                dfg
+                dfg
+                dfg
+                fdgs
+                sdgf
+                sdagf
+                s
+                ddafsdf
+                gfd
+                gsfdg
+                fsd
+                sd
+                sgf
+                dfg
+                dfg
+                dfg
+                fdgs
+                sdgf
+                sdagf
+                s
+                dsfdasg
+                dsfdasgasdgf
+                afdg
+                ag
+                sdgfadg
+                agrf
+                sdf
+                afdgadrgf
+                afdgadrgfadg
+                dsfdasgasdgf
+                sdgfadgfg
+                fg
+                dfg
+                sdgf
+                dfgddafsdf
+                gfd
+                gsfdg
+                fsd
+                sd
+                sgf
+                dfg
+                dfg
+                dfg
+                fdgs
+                sdgf
+                sdagf
+                s
+                dsfdasg
+                dsfdasgasdgf
+                afdg
+                ag
+                sdgfadg
+                agrf
+                sdf
+                afdgadrgf
+                afdgadrgfadg
+                dsfdasgasdgf
+                sdgfadgfg
+                fg
+                dfg
+                sdgf
+                dfg
+                ddafsdf
+                gfd
+                gsfdg
+                fsd
+                sd
+                sgf
+                dfg
+                dfg
+                dfg
+                fdgs
+                sdgf
+                sdagf
+                s
+                dsfdasg
+                dsfdasgasdgf
+                afdg
+                ag
+                sdgfadg
+                agrf
+                sdf
+                afdgadrgf
+                afdgadrgfadg
+                dsfdasgasdgf
+                sdgfadgfg
+                fg
+                dfg
+                sdgf
+                dfg
+              </p>
+            </section>
+            <section id="projects" className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24">
+              <h2 className="text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only">Projects</h2>
+              <p className="mt-4 text-slate-400">Descripción de los proyectos realizados.</p>
+              <p>sdfdssssssssssssssssssssssssssssssssssssssssss
+                asdfaf
+                asdfafasdf
+                ddafsdf
+                gfd
+                gsfdg
+                fsd
+                sd
+                sgf
+                dfg
+                dfg
+                dfg
+                fdgs
+                sdgf
+                sdagf
+                s
+                dsfdasg
+                dsfdasgasdgf
+                afdg
+                ag
+                sdgfadg
+                agrf
+                sdf
+                afdgadrgf
+                afdgadrgfadg
+                dsfdasgasdgf
+                sdgfadgfg
+                fg
+                dfg
+                sdgf
+                dfg
+                ddafsdf
+                gfd
+                gsfdg
+                fsd
+                sd
+                sgf
+                dfg
+                dfg
+                dfg
+                fdgs
+                sdgf
+                sdagf
+                s
+                dsfdasg
+                dsfdasgasdgf
+                afdg
+                ag
+                sdgfadg
+                agrf
+                sdf
+                afdgadrgf
+                afdgadrgfadg
+                dsfdasgasdgf
+                sdgfadgfg
+                fg
+                dfg
+                sdgf
+                dfg
+                ddafsdf
+                gfd
+                gsfdg
+                fsd
+                sd
+                sgf
+                dfg
+                dfg
+                dfg
+                fdgs
+                sdgf
+                sdagf
+                s
+                dsfdasg
+                dsfdasgasdgf
+                afdg
+                ag
+                sdgfadg
+                agrf
+                sdf
+                afdgadrgf
+                afdgadrgfadg
+                dsfdasgasdgf
+                sdgfadgfg
+                fg
+                dfg
+                sdgf
+                dfg
+                ddafsdf
+                gfd
+                gsfdg
+                fsd
+                sd
+                sgf
+                dfg
+                dfg
+                dfg
+                fdgs
+                sdgf
+                sdagf
+                s
+                dsfdasg
+                dsfdasgasdgf
+                afdg
+                ag
+                sdgfadg
+                agrf
+                sdf
+                afdgadrgf
+                afdgadrgfadg
+                dsfdasgasdgf
+                sdgfadgfg
+                fg
+                dfg
+                sdgf
+                dfg
+                ddafsdf
+                gfd
+                gsfdg
+                fsd
+                sd
+                sgf
+                dfg
+                dfg
+                dfg
+                fdgs
+                sdgf
+                sdagf
+                s
+                dsfdasg
+                dsfdasgasdgf
+                afdg
+                ag
+                sdgfadg
+                agrf
+                sdf
+                afdgadrgf
+                afdgadrgfadg
+                dsfdasgasdgf
+                sdgfadgfg
+                fg
+                dfg
+                sdgf
+                dfg
+                
+              </p>
+            </section>
+          </main>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
